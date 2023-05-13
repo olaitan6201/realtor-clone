f55995cc67953b97cae276a937b301d868f12f91
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-13 12:08:34 +0100

    created Pages and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
-  return (
-    <div>
-      <h1>Welcome to Realtor</h1>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route
+					path='/'
+					element={<Home />}
+				/>
+				<Route
+					path='/sign-in'
+					element={<SignIn />}
+				/>
+				<Route
+					path='/sign-up'
+					element={<SignUp />}
+				/>
+				<Route
+					path='/profile'
+					element={<Profile />}
+				/>
+				<Route
+					path='/forgot-password'
+					element={<ForgotPassword />}
+				/>
+				<Route
+					path='/offers'
+					element={<Offers />}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
