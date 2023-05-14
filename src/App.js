8f3ce5c6087ff3ccf1db3a28eed9cea3ec5b7bd0
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-14 15:31:24 +0100

    installed and configured react-toastify

--- src/App.js
+++ src/App.js
@@ -7,10 +7,24 @@ import Profile from './pages/Profile';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 	return (
 		<div>
+			<ToastContainer
+				position='bottom-center'
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				pauseOnHover
+				draggable
+				theme='dark'
+			/>
 			<Header />
 			<Routes>
 				<Route
