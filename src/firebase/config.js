bac51a8b68bb364d04c3d169c46a67805bcc5a24
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-14 14:29:30 +0100

    installed and configured firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBdnUNfm_M_-Lt4Me5xovu7UFe66vy2wyk",
+    authDomain: "react-realtor-clone-8238e.firebaseapp.com",
+    projectId: "react-realtor-clone-8238e",
+    storageBucket: "react-realtor-clone-8238e.appspot.com",
+    messagingSenderId: "133719121037",
+    appId: "1:133719121037:web:97b58aa29a046bcdc396c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore()
