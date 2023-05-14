30e8cb04d9758cd5e2969f88faa46a44ed783dda
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-14 21:25:22 +0100

    completed the Header component, making it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,47 @@
 import { useLocation, useNavigate } from "react-router"
 import PageRoute from "./PageRoute"
+import { useAuthStatus } from "../hooks/useAuthStatus"
+import { useEffect, useState } from "react"
+import { auth } from "../firebase/config"
+import { onAuthStateChanged } from "firebase/auth"
 
 export default function Header() {
     const location = useLocation()
     const navigate = useNavigate()
+    const [pageState, setPageState] = useState({ title: 'Sign In', url: '/sign-in' })
     const pageRoutes = [
         {
             title: "Home",
             url: '/',
-            alt: ''
+            alt: []
         },
         {
             title: "Offers",
             url: '/offers',
-            alt: ''
+            alt: []
         },
         {
-            title: "Sign In",
-            url: '/sign-in',
-            alt: '/sign-up'
+            title: pageState.title,
+            url: pageState.url,
+            alt: [
+                '/sign-in',
+                '/sign-up',
+                '/profile',
+                'forgot-password'
+            ]
         }
     ]
 
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setPageState({ title: 'Profile', url: '/profile' })
+            } else {
+                setPageState({ title: 'Sign In', url: '/sign-in' })
+            }
+        })
+    }, [auth])
+    
     return (
         <header className="bg-white border-b shadow-sm sticky top-0 z-50">
             <div className="flex justify-between items-center px-3 max-w-6xl mx-auto">
@@ -35,7 +55,10 @@ export default function Header() {
                 <div>
                     <ul className="flex space-x-10">
                         {pageRoutes?.map((r, i) =>
-                            <PageRoute key={i} route={r} location={location} />
+                            <PageRoute
+                                key={i} route={r}
+                                location={location}
+                            />
                         )}
                     </ul>
                 </div>
