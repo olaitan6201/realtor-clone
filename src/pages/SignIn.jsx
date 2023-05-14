248f72449bc99964d7aa40db502e4aa9daa7c2c8
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-14 14:03:34 +0100

    updated SignIn page component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react"
+import Input from "../components/Input"
+import { Link } from "react-router-dom"
+import OAuth from "../components/OAuth"
+
 export default function SignIn() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const { email, password } = formData
+
+    const handleInput = (e) => {
+        const { id, value } = e.target
+        setFormData((prevState) => ({
+            ...prevState,
+            [id]: value
+        }))
+    }
+
     return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
+        <section>
+            <h1 className="text-3xl text-center mt-6 font-extrabold">Sign In</h1>
+
+            <div className="flex justify-center items-center flex-wrap px-6 py-12 max-w-6xl mx-auto">
+                <div className="hidden md:inline-block md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+                    <img
+                        src="/key_logo.avif"
+                        alt="key"
+                        className="w-full rounded-2xl "
+                    />
+                </div>
+                <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+                    <form>
+                        <Input
+                            type="email" id="email"
+                            value={email}
+                            event={handleInput}
+                            placeholder="Email address"
+                        />
+                        <Input
+                            id="password"
+                            value={password}
+                            event={handleInput}
+                            isPassword
+                        />
+                        <div className="my-3 flex justify-between whitespace-nowrap text-sm sm:text-md">
+                            <p>Don't have an account?
+                                <Link to={'/sign-up'} className="text-red-600 hover:text-red-700 auth-link pl-2">Register</Link>
+                            </p>
+                            <Link to={'/forgot-password'} className="text-blue-600 hover:text-blue-700 auth-link pl-2">Forgot Password?</Link>
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-500 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800"
+                        >Sign In</button>
+                    </form>
+                    <div className="my-4 items-center flex before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+                        <p className="text-center font-semibold mx-4 ">OR</p>
+                    </div>
+                    <OAuth />
+                </div>
+            </div>
+        </section>
     )
 }
