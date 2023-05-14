c4ef2384fa9acef4af95d7d142bb198bc16d4fc4
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-14 18:10:29 +0100

    created useAuthStatus hook for cheking user logged in status

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,17 @@
+import { onAuthStateChanged } from "firebase/auth"
+import { useEffect, useState } from "react"
+import { auth } from "../firebase/config"
+
+export const useAuthStatus = () => {
+	const [loggedIn, setLoggedIn] = useState(false)
+	const [checkingStatus, setCheckingStatus] = useState(true)
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) setLoggedIn(true)
+			setCheckingStatus(false)
+		})
+	}, [])
+
+	return { loggedIn, checkingStatus }
+}
