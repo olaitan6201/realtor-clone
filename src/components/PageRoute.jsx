0d9190403c51d1219b25622da4a753d4fb1415f0
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-14 21:24:58 +0100

    modified PageRoute component

--- src/components/PageRoute.jsx
+++ src/components/PageRoute.jsx
@@ -2,9 +2,10 @@ import { useNavigate } from "react-router"
 
 export default function PageRoute({ route, location }) {
     const { title, url, alt } = route
-    const pathMatchRoute = (route) => route === location.pathname
+    const pathMatchRoute = () => url === location.pathname || alt.includes(location.pathname)
     const navigate = useNavigate()
+
     return (
-        <li className={`link ${(pathMatchRoute(url) || pathMatchRoute(alt)) ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"}`} key={url} onClick={() => navigate(url)}>{title}</li>
+        <li className={`link ${pathMatchRoute() ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"}`} key={url} onClick={() => navigate(url)}>{title}</li>
     )
 }
