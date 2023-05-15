6394ea5f5fa167d16c4acaed6a176040fa6ff240
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-15 18:19:51 +0100

    modified Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,91 +1,19 @@
-import { useState } from 'react'
-import Input from '../components/Input'
-import { auth, db } from '../firebase/config'
-import { useNavigate } from 'react-router'
-import { toast } from 'react-toastify'
-import { updateProfile } from 'firebase/auth'
-import { doc, updateDoc } from 'firebase/firestore'
+import { Link } from "react-router-dom";
+import ProfileForm from "./ProfileForm";
+import { FcHome } from "react-icons/fc";
+
 export default function Profile() {
-	const { currentUser } = auth
-
-	const navigate = useNavigate()
-	const [formData, setFormData] = useState({
-		name: currentUser?.displayName,
-		email: currentUser?.email
-	})
-	const [updateEnabled, setUpdateEnabled] = useState(false)
-
-	const { email, name } = formData
-
-	const onLogOut = () => {
-		auth.signOut()
-
-		toast.success('Sign out successful!')
-
-		navigate('/')
-	}
-
-	const handleInput = (e) => {
-		const { id, value } = e.target
-		setFormData((prevState) => ({
-			...prevState,
-			[id]: value
-		}))
-	}
-
-	const handleUpdate = async () => {
-		if (updateEnabled) await handleSubmit()
-		setUpdateEnabled((prevState) => !prevState)
-	}
-
-	const handleSubmit = async () => {
-		try {
-			if (currentUser.displayName !== name) {
-				await updateProfile(currentUser, {
-					displayName: name
-				})
-
-				const docRef = doc(db, 'users', currentUser.uid)
-				await updateDoc(docRef, { name })
-
-				return toast.success('Profile updated successfully!')
-			}
-			return toast.info('No update found!')
-		} catch (error) {
-			toast.error('Unable to update profile details!')
-		}
-	}
 
 	return (
 		<section className='max-w-6xl mx-auto flex flex-col justify-center items-center'>
 			<h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
 			<div className="w-full md:w-[50%] mt-6 px-3">
-				<form>
-					<div className="my-3">
-						<Input
-							event={handleInput}
-							readOnly={!updateEnabled}
-							type='text' id='name'
-							value={name}
-							extraClass={`${updateEnabled && "bg-red-200 focus:bg-red-200 focus:ring-0"}`}
-						/>
-					</div>
-					<div className="my-3">
-						<Input
-							readOnly type='email'
-							id='email' value={email}
-						/>
-					</div>
-					<div className="flex justify-between items-center whitespace-nowrap text-sm sm:text-lg space-y-3">
-						<p className='flex items-center space-x-2'>
-							<span>Do you want to change your name?</span>
-							<span className='text-red-600 cursor-pointer hover:text-red-800 transition duration-200 ease-in-out' onClick={handleUpdate}>
-								{updateEnabled ? "Apply change" : "Edit"}
-							</span>
-						</p>
-						<p onClick={onLogOut} className='text-blue-600 hover:text-blue-800 cursor-pointer transition duration-200 ease-in-out'>Sign Out</p>
-					</div>
-				</form>
+				<ProfileForm />
+
+				<Link to={'/create-listing'} className="w-full bg-blue-600 hover:bg-blue-600 active:bg-blue-800 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md transition duration-200 ease-in-out hover:shadow-lg flex justify-center items-center space-x-3">
+					<FcHome className="text-3xl bg-red-200 rounded-full p-1 border-2" />
+					<span>Sell or Rent your Home</span>
+				</Link>
 			</div>
 		</section>
 	)
