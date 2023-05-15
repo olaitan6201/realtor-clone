a6bf69bb554a198d3f2eaba560b74dd55788e79d
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-15 18:19:31 +0100

    created Custom Input component

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,33 @@
+export default function CustomInput({
+    title = '', extraClass = '',
+    subTitle='', useTextArea = false,
+    event, ...props
+}) {
+    const handleInput = (e) => {
+        const { id, value } = e.target
+        event({ id, value })
+    }
+
+    return (
+        <div className="my-3 space-y-1">
+            <p className="text-lg mt-6 font-semibold">{title}</p>
+            <small className="text-gray-600 font-medium">{subTitle}</small>
+            {useTextArea ? (
+                <textarea
+                    {...props}
+                    onChange={handleInput}
+                    className={`input ${extraClass}`}
+                    style={{
+                        resize: "none"
+                    }}
+                ></textarea>
+            ) : (
+                <input
+                    {...props}
+                    onChange={handleInput}
+                    className={`input ${extraClass}`}
+                />
+            )}
+        </div>
+    )
+}
